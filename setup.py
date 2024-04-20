bcba04a9218d2593d2ad9dd52d4faa72795bf7d5
pakkinlau <kinlau529@gmail.com>
2024-04-20 17:04:07 +0800

    updating vc scripts, again

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='BookDB',
     version='0.1.1',
     description='A tool for populating book data into databases for big data processing',
-    long_description=open('README.md', encoding='utf-8').read(),
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Pak Kin Lau',
     author_email='pakkinlau.general@gmail.com',
@@ -17,12 +17,8 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
     ],
 )
