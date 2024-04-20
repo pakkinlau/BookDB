a5df0aec6ab2e028ab0af16898d30b455fe3ba29
pakkinlau <kinlau529@gmail.com>
2024-04-20 16:44:47 +0800

    updating setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BookDB',
+    version='0.1.1',
+    description='A tool for populating book data into databases for big data processing',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Pak Kin Lau',
+    author_email='pakkinlau.general@gmail.com',
+    url='https://github.com/pakkinlau/BookDB',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    install_requires=[
+        # List your package dependencies here
+        # 'requests', 'pandas', etc.
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
